fff464797f5ef93339290eea31a4568e1816a138
samyakjain09 <samyakjain1p@gmail.com>
2026-08-21 18:10:21 +0530

    Add variable initialization and array declaration in Basics.java

--- OOPS/Basics.java
+++ OOPS/Basics.java
@@ -1,5 +1,9 @@
 class Basics{
     public static void main(String[] args){
         System.out.println("Hello World");
+        int num1=7;
+        int num2=num1++;
+        System.out.println(num1);
+        int[][] arr={{2,4},};
     }
 }
