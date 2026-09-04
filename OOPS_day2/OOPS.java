d176a275894a033877c6667f269b3baf68837959
samyakjain09 <samyakjain1p@gmail.com>
2026-09-04 21:38:32 +0530

    Enhance BankAccount

--- OOPS_day2/OOPS.java
+++ OOPS_day2/OOPS.java
@@ -13,11 +13,28 @@ class BankAccount{
     public double checkBalance(){
         return balance;
     }
+    public void depositAmount(double amount){
+        if(amount>0){
+            balance+=amount;
+        }else{
+            System.out.println("amount should be positive");
+        }
+    }
+    public void withdrawAmount(double amount){
+        if(amount<=balance){
+            balance-=amount;
+        }else{
+            System.out.println("insufficient balance");
+        }
+    }
 }
 public class OOPS{
     public static void main(String[] args){
         BankAccount e1=new BankAccount("samyak",1000);
         e1.setname("samyak jain");
+        System.out.println(e1.checkBalance());;
+        e1.withdrawAmount(500);
+        e1.depositAmount(20000);
         System.out.println(e1.checkBalance());;
     }
 }
